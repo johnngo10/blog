b1095cc868ea165de975f1b7145e15f5f2a15447
xuanngo10 <xuan.ngo10@gmail.com>
2021-08-31 15:57:13 -0700

    added pagination to main page and user posts page

--- client/src/components/Main.js
+++ client/src/components/Main.js
@@ -1,15 +1,19 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
 import { bindActionCreators } from "redux";
 import moment from "moment";
+import ReactPaginate from "react-paginate";
 import * as postActions from "../actions/post_actions";
 
 const Main = () => {
+  const [pageNumber, setPageNumber] = useState(0);
   const dispatch = useDispatch();
+  const postsPerPage = 10;
+  const pagesVisited = pageNumber * postsPerPage;
 
   const state = useSelector((state) => state);
-  const { all, newPosts } = state.posts;
+  const { newPosts, all } = state.posts;
 
   const { fetchPosts } = bindActionCreators(postActions, dispatch);
 
@@ -17,27 +21,46 @@ const Main = () => {
     fetchPosts();
   }, [newPosts]);
 
+  const displayPosts = Object.values(all)
+    .slice(pagesVisited, pagesVisited + postsPerPage)
+    .map((value, index) => {
+      return (
+        <div className="card-container" key={index}>
+          <Link to={`/post/${value._id}`} className="card-link">
+            <p className="card-title">{value.title}</p>
+            <p className="card-author">Author: {value.author.username}</p>
+            <p className="card-date">
+              {moment(value.date).format("MMMM Do YYYY")}
+            </p>
+          </Link>
+        </div>
+      );
+    });
+
+  const pageCount = Math.ceil(all.length / postsPerPage);
+
+  const changePage = ({ selected }) => {
+    setPageNumber(selected);
+  };
+
   return (
     <div className="main-container">
       {Object.values(all).length === 0 ? (
         <div className="no-post-message">There are no Posts</div>
       ) : (
-        <div className="cards">
-          {Object.values(all).map((value, index) => {
-            return (
-              <div className="card-container" key={index}>
-                <Link to={`/post/${value._id}`} className="card-link">
-                  <p className="card-title">{value.title}</p>
-                  <p className="card-author">Author: {value.author.username}</p>
-                  <p className="card-date">
-                    {moment(value.date).format("MMMM Do YYYY")}
-                  </p>
-                </Link>
-              </div>
-            );
-          })}
-        </div>
+        <div className="cards">{displayPosts}</div>
       )}
+      <ReactPaginate
+        previousLabel={"Previous"}
+        nextLabel={"Next"}
+        pageCount={pageCount}
+        onPageChange={changePage}
+        containerClassName={"pagination-btn"}
+        previousLinkClassName={"previous-btn"}
+        nextLinkClassName={"next-btn"}
+        disabledClassName={"pagination-disabled"}
+        activeClassName={"pagination-active"}
+      />
     </div>
   );
 };
--- client/src/components/Post/UserPosts.js
+++ client/src/components/Post/UserPosts.js
@@ -1,16 +1,20 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { useParams, Link } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
 import { bindActionCreators } from "redux";
 import moment from "moment";
+import ReactPaginate from "react-paginate";
 import * as postActions from "../../actions/post_actions";
 
 const UserPosts = () => {
+  const [pageNumber, setPageNumber] = useState(0);
   const dispatch = useDispatch();
   const { id } = useParams();
+  const postsPerPage = 10;
+  const pagesVisited = pageNumber * postsPerPage;
 
   const state = useSelector((state) => state);
-  const { user } = state.posts;
+  const posts = state.posts.user;
 
   const { fetchUserPosts } = bindActionCreators(postActions, dispatch);
 
@@ -18,27 +22,46 @@ const UserPosts = () => {
     fetchUserPosts(id);
   }, []);
 
+  const displayPosts = Object.values(posts)
+    .slice(pagesVisited, pagesVisited + postsPerPage)
+    .map((value, index) => {
+      return (
+        <div className="card-container" key={index}>
+          <Link to={`/post/${value._id}`} className="card-link">
+            <p className="card-title">{value.title}</p>
+            <p className="card-author">Author: {value.author.username}</p>
+            <p className="card-date">
+              {moment(value.date).format("MMMM Do YYYY")}
+            </p>
+          </Link>
+        </div>
+      );
+    });
+
+  const pageCount = Math.ceil(posts.length / postsPerPage);
+
+  const changePage = ({ selected }) => {
+    setPageNumber(selected);
+  };
+
   return (
     <div className="main-container">
-      {Object.values(user).length === 0 ? (
+      {Object.values(posts).length === 0 ? (
         <div className="no-post-message">There are no posts</div>
       ) : (
-        <div className="cards">
-          {Object.values(user).map((value, index) => {
-            return (
-              <div className="card-container" key={index}>
-                <Link to={`/post/${value._id}`} className="card-link">
-                  <p className="card-title">{value.title}</p>
-                  <p className="card-author">Author: {value.author.username}</p>
-                  <p className="card-date">
-                    {moment(value.date).format("MMMM Do YYYY")}
-                  </p>
-                </Link>
-              </div>
-            );
-          })}
-        </div>
+        <div className="cards">{displayPosts}</div>
       )}
+      <ReactPaginate
+        previousLabel={"Previous"}
+        nextLabel={"Next"}
+        pageCount={pageCount}
+        onPageChange={changePage}
+        containerClassName={"pagination-btn"}
+        previousLinkClassName={"previous-btn"}
+        nextLinkClassName={"next-btn"}
+        disabledClassName={"pagination-disabled"}
+        activeClassName={"pagination-active"}
+      />
     </div>
   );
 };

224539f437183b04d12fdcea3b638759727fd60b
xuanngo10 <xuan.ngo10@gmail.com>
2021-08-31 22:13:23 -0700

    getting ready to deploy to heroku

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const cookieParser = require("cookie-parser");
 const app = express();
+const path = require("path");
 const session = require("express-session");
 const passport = require("passport");
 require("./config/passport")(passport);
@@ -38,6 +39,15 @@ mongoose
 
 app.use("/api/user", userRouter);
 app.use("/api/post", postRouter);
+
+// Serve static assets in production
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
 
 app.listen(process.env.PORT || 5000, () => {
   console.log("Your app is listening on port 5000");
